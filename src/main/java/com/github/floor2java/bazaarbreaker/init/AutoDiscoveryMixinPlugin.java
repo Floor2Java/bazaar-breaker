e1560986632ab16b31f5e0c4298ac87bf07b61fe
GitHub Action <action@github.com>
2025-03-28 18:34:28 +0000

    Initialize template repository proper
    Set package name to com.github.floor2java.bazaarbreaker
    Set project name to bazaar-breaker
    Set modid to bazaarbreaker

--- src/main/java/com/github/floor2java/bazaarbreaker/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/floor2java/bazaarbreaker/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.floor2java.bazaarbreaker.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
